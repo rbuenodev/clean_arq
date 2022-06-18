1dca91632c5be502fd46b133ada1ceb22e873b51
Richard <richard_bueno@ymail.com>
2022-06-18 18:08:54 -0300

    correcao desafio

--- src/usecase/product/update/update.product.integration.spec.ts
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize-typescript";
 import Product from "../../../domain/product/entity/product";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
-import ProductFindUseCase from "./find.product.usecase";
+import ProductUpdateUseCase from "./update.product.usecase";
 
-describe("Integration test to find a product", () => {
+describe("Integration test update product usecase", () => {
   let sequelize: Sequelize;
 
   beforeEach(async () => {
@@ -23,32 +23,20 @@ describe("Integration test to find a product", () => {
     await sequelize.close();
   });
 
-  it("Should find a product", async () => {
+  it("Should update a product", async () => {
     const productRepository = new ProductRepository();
-    const productUseCase = new ProductFindUseCase(productRepository);
-
     const product = new Product("123", "product", 10);
     await productRepository.create(product);
 
-    const input = { id: "123" };
+    const productUseCase = new ProductUpdateUseCase(productRepository);
 
-    const result = await productUseCase.execute(input);
-    const output = {
+    const input = {
       id: "123",
-      name: "product",
-      price: 10,
+      name: "product2",
+      price: 20,
     };
 
-    expect(result).toEqual(output);
-  });
-
-  it("should not find a product", async () => {
-    const productRepository = new ProductRepository();
-
-    const usecase = new ProductFindUseCase(productRepository);
-    const input = { id: "1234" };
-    expect(() => {
-      return usecase.execute(input);
-    }).rejects.toThrow("Product not found");
+    const output = await productUseCase.execute(input);
+    expect(output).toEqual(input);
   });
 });
